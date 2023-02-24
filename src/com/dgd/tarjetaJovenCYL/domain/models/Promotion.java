46f5645bafa064235992c0ad32ab5fe75979f190
diegogd04 <diego2612004@gmail.com>
2023-02-24 17:00:24 +0100

    Crear clase Promotion

---
+++ src/com/dgd/tarjetaJovenCYL/domain/models/Promotion.java
@@ -0,0 +1,50 @@
+package com.dgd.tarjetaJovenCYL.domain.models;
+
+public class Promotion {
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer discount;
+
+    private Promotion promotion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+}
