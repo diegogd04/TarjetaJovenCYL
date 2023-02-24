89be787ee97aee8344cbfd680d023177665989a9
diegogd04 <diego2612004@gmail.com>
2023-02-24 17:01:48 +0100

    Crear clase User

---
+++ src/com/dgd/tarjetaJovenCYL/domain/models/User.java
@@ -0,0 +1,40 @@
+package com.dgd.tarjetaJovenCYL.domain.models;
+
+public class User {
+    private String nif;
+    private String name;
+    private String surnames;
+    private String address;
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
