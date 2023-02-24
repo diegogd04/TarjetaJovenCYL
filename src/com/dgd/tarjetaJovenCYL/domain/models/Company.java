9c9593badb8143592bdc1f30f28d1cc35a2c5e59
diegogd04 <diego2612004@gmail.com>
2023-02-24 16:56:30 +0100

    Crear clase Company

---
+++ src/com/dgd/tarjetaJovenCYL/domain/models/Company.java
@@ -0,0 +1,31 @@
+package com.dgd.tarjetaJovenCYL.domain.models;
+
+public class Company {
+    private Integer id;
+    private String name;
+    private String cif;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+}
